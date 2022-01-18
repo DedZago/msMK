0239f0a6e43d27e1c6b6ff93dde7f3d746e9b8e9
Daniele Zago <zagodaniele.9@gmail.com>
2022-01-18 08:48:44 +0100

    more tests

--- tests/estimation/run_all_DP.sh
+++ tests/estimation/run_all_DP.sh
@@ -2,47 +2,47 @@
 
 # Change key values and write results back to settings.json (recreate it with result).
 
-DensArray=("6" "7" "8" "9" "10" "11" "12")
-nArray=(250 500 1000)
-pArray=(2 5 10)
-for dens in ${DensArray[*]}; do
-    for n in ${nArray[*]}; do
-        for p in ${pArray[*]}; do
-            jq -c ".gen = \"r${dens}\"" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-            jq -c ".df = \"f${dens}\"" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-            jq -c ".p = ${p}" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-            jq -c ".n = ${n}" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-            Rscript simulate_DP.R &
-            sleep 2s
-            pwait 6
-        done
-    done
-done
-# jq -c '.gen = "r6"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f6"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r7"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f7"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r8"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f8"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r9"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f9"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r10"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f10"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r11"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f11"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# sleep 5s
-# jq -c '.gen = "r12"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# jq -c '.df = "f12"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
-# gnome-terminal -- Rscript simulate_DP.R &
-# wait
+# DensArray=("6" "7" "8" "9" "10" "11" "12")
+# nArray=(250 500 1000)
+# pArray=(2 5 10)
+# for dens in ${DensArray[*]}; do
+#     for n in ${nArray[*]}; do
+#         for p in ${pArray[*]}; do
+#             jq -c ".gen = \"r${dens}\"" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+#             jq -c ".df = \"f${dens}\"" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+#             jq -c ".p = ${p}" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+#             jq -c ".n = ${n}" settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+#             Rscript simulate_DP.R &
+#             sleep 2s
+#             pwait 6
+#         done
+#     done
+# done
+jq -c '.gen = "r6"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f6"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r7"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f7"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r8"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f8"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r9"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f9"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r10"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f10"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r11"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f11"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+sleep 5s
+jq -c '.gen = "r12"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+jq -c '.df = "f12"' settings.json > tmp.$$.json && mv tmp.$$.json settings.json
+gnome-terminal -- Rscript simulate_DP.R &
+wait
